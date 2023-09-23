b2fb15bd67948da6be67624e258a3598ed80c88c
Rauf Yılmaz <107587062+Rofubtw@users.noreply.github.com>
2023-09-23 06:20:47 +0300

    Testing State Machine

---
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestState : PlayerBaseState
+{
+    private float timeRemaining;
+    public PlayerTestState(PlayerStateMachine stateMachine, float timerLimit) : base(stateMachine)
+    {
+        this.timeRemaining = timerLimit;
+    }
+
+    public override void Enter()
+    {
+        Debug.Log("Enter");
+    }
+
+    public override void Tick(float deltaTime)
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            Debug.Log(timeRemaining);
+        }
+        else
+        {
+            stateMachine.SwitchState(new PlayerTestState(stateMachine, 5f));
+        }
+    }
+
+    public override void Exit()
+    {
+        Debug.Log("Exit");
+    }
+}
