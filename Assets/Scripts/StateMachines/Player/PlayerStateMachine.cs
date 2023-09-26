3857fbcb83115b71177b5e0a96098c68e42b5ea3
Rauf Yılmaz <107587062+Rofubtw@users.noreply.github.com>
2023-09-26 04:54:08 +0300

    PlayerModel

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class PlayerStateMachine : StateMachine
 {
+    [field: SerializeField] public InputReader InputReader { get; private set; }
     private void Start()
     {
-        SwitchState(new PlayerTestState(this,3));
+        SwitchState(new PlayerTestState(this, 3));
     }
 }
--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -13,23 +13,32 @@ public class PlayerTestState : PlayerBaseState
     public override void Enter()
     {
         Debug.Log("Enter");
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
 
     public override void Tick(float deltaTime)
     {
+        
+        
         if (timeRemaining > 0)
         {
             timeRemaining -= Time.deltaTime;
-            Debug.Log(timeRemaining);
+            //Debug.Log(timeRemaining);
         }
         else
         {
-            stateMachine.SwitchState(new PlayerTestState(stateMachine, 5f));
+            //stateMachine.SwitchState(new PlayerTestState(stateMachine, 5f));
         }
+    }
+
+    private void OnJump()
+    {
+        stateMachine.SwitchState(new PlayerTestState(stateMachine,10f));
     }
 
     public override void Exit()
     {
         Debug.Log("Exit");
+        stateMachine.InputReader.JumpEvent -= OnJump;
     }
 }
